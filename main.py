50a9d95b335cd4575f952df0c228e798daf52a1f
Arya <jackrack040@gmail.com>
2024-10-20 10:59:59 -0400

    delete posts

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ class Post(BaseModel):
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "i like pizza", "id": 2}]
 
-def get_post_from_id(id) :
+def get_post_from_id(id):
     for post in my_posts:
         if post["id"] == id:
             return post
+
+def delete_post_from_id(id):
+    for idx in range(len(my_posts)):
+        if my_posts[idx]["id"] == id:
+            my_posts.pop(idx)
 
 # Known as a path operation or route:
 @app.get("/") # decorator (@), fastapi instance (app), hppt method (.get), route path ("/")
@@ -36,10 +41,15 @@ def create_posts(payload: Post):
     return post_dict
 
 @app.get("/posts/{id}") # this id field is known as a path parameter
-def get_one_post(id: int, response: Response):
+def get_one_post(id: int):
     post = get_post_from_id(id)
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found!")
     else:
         return post
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    delete_post_from_id(id)
+    return {"Message": "Post deleted!"}
