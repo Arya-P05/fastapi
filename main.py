2bdedcec4d1b8568e968debe8e5b0c8dec2b4bea
Arya <jackrack040@gmail.com>
2024-10-20 10:35:59 -0400

    basics of get and post

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from random import randint
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    public: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "i like pizza", "id": 2}]
+
+def get_post_from_id(id) :
+    for post in my_posts:
+        if post["id"] == id:
+            return post
 
 # Known as a path operation or route:
 @app.get("/") # decorator (@), fastapi instance (app), hppt method (.get), route path ("/")
 def root():
     return {"message": "hi chakli"}
 
-@app.get("/get_posts/")
+@app.get("/posts/")
 def get_posts():
-    return {"data": "post details"}
+    return {"data": my_posts}
+
+@app.post("/posts/")
+def create_posts(payload: Post):
+    # return {"title": f"{payload.title}", "content": f"{payload.content}", "is_post_public": payload.public, "rating": payload.rating}
+    post_dict = payload.model_dump()
+    post_dict["id"] = randint(0, 1000000)
+    my_posts.append(post_dict)
+    return post_dict
+
+@app.get("/posts/{id}") # this id field is known as a path parameter
+def get_one_post(id: int):
+    post = get_post_from_id(id)
+    return post
