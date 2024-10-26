5c9e01e11c9ee952a73ec53b87528db8f3c232af
Arya <jackrack040@gmail.com>
2024-10-26 13:20:44 -0400

    done basics of crud app, time to build fr

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ class Post(BaseModel):
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "i like pizza", "id": 2}]
 
 def get_post_from_id(id):
-    for post in my_posts:
+    for idx, post in enumerate(my_posts):
         if post["id"] == id:
-            return post
+            return idx
+    
+    return "not found"
 
 def delete_post_from_id(id):
     for idx in range(len(my_posts)):
@@ -42,7 +44,7 @@ def create_posts(payload: Post):
 
 @app.get("/posts/{id}") # this id field is known as a path parameter
 def get_one_post(id: int):
-    post = get_post_from_id(id)
+    post = my_posts[get_post_from_id(id)]
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found!")
@@ -51,5 +53,21 @@ def get_one_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
+    if get_post_from_id(id) == "not found":
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found.")
     delete_post_from_id(id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    post_idx = get_post_from_id(id)
+    
+    if post_idx == "not found":
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found.")
+    
+    new_updated_post = post.model_dump()
+    new_updated_post["id"] = id
+
+    my_posts[post_idx] = new_updated_post
+    return {"data": new_updated_post}
+
