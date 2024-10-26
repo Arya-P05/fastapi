4f575593e6031064603b1c0c228cc7bb316ae7da
Arya <jackrack040@gmail.com>
2024-10-26 13:21:49 -0400

    notes

--- main.py
+++ main.py
@@ -71,3 +71,36 @@ def update_post(id: int, post: Post):
     my_posts[post_idx] = new_updated_post
     return {"data": new_updated_post}
 
+
+
+'''
+
+coding with john
+nana
+
+boost library 
+layer called swig - python and exel 
+servies runnin gon java and jquery 
+jenkin
+docker
+asqwer and aws
+argo and hera 
+python 3.11 
+multithreading lots of it
+opening and closing data and connections
+felix - codebase older than me 
+
+pay attention to thoery courses - deterministic and non, 
+theory of cs is very important 
+
+invest a lot of time into multithreading and embedded systems to high level processes for automation 
+what is proper multithreading 
+how do you shift locations, etc etcc
+
+look more into canadian companies because we pump out a lot more interesting products 
+
+core of uber eats and ride sharing was made in toronto 
+
+blink
+
+'''
